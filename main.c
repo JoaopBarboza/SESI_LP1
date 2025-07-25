26b44a07f8b1f91d1d621d3c7ca04a6a3fa21690
JoaopBarboza <joaopedro090709@gmail.com>
2025-07-25 10:55:49 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+void main(){
+    printf("Se quiser sim mano🦕🦕🦕.\n");
+    printf("Bom Dia minha casada💘💘.\n");
+    
+    int X;
+    int Y;
+    X=1;
+    Y=2;
+    printf("%b %b", X, Y);
+}
