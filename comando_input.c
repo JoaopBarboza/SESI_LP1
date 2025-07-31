0f3ec55dacbe9b900467b6cc1407057a04662308
João Barboza <joao.pedro090709@gmail.com>
2025-07-31 11:34:56 -0300

    comando de entrada

---
+++ comando_input.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int resultado = a * b;
+    
+    printf("Digite o valor de a: ");
+    scanf("%d", &a);
+    printf("Digite valor de b:");
+    scanf("%d", &b);
+    
+    int x = a * b;
+    
+    printf("Fala Big.👃👃👃\n");
+    printf("%d x %d = %d", a ,b, resultado);
+  
+}
