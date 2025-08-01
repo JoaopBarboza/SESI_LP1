16e56033b54e25f8fe230375719cc217c517decc
Joao barboza <joao.pedro090709@gmail.com>
2025-08-01 11:46:21 -0300

    lista de exercicios

---
+++ entrda de dados EX/ex_ 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int resultado = a + b;
+    
+    printf("Digite o valor de a:");
+    scanf("%d", &a);
+    printf("Digite valor de b:");
+    scanf("%d", &b);
+    
+    resultado = a + b;
+    
+    printf("Olá Casada❤️❤️❤️\n");
+    printf("%d + %d = %d", a ,b, resultado);
+  
+}
